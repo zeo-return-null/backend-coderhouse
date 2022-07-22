b2965051d85bbd980aa09f2030db409ba576c35e
Juan Sepulveda <cjfsepu@protonmail.com>
2022-07-21 21:27:43 -0300

    añadiendo desafio 13

---
+++ Desafio 13/src/config/mysql.js
@@ -0,0 +1,40 @@
+const Knex = require("knex").default;
+const path = require("path");
+let optionsPath = path.join(__dirname, "..", "options.js");
+const { optionsMySQL } = require("../options.js");
+const knex = Knex({
+	client: "mysql2",
+	connection: optionsMySQL,
+});
+
+const init = async () => {
+	await knex.schema.dropTableIfExists("productos");
+	await knex.schema.createTable("productos", (table) => {
+		table.increments("id").primary().notNullable();
+		table.string("title", 80).notNullable();
+		table.float("price").notNullable();
+		table.string("thumbnail", 250).notNullable();
+	});
+	await knex("productos").insert([
+		{ 
+			title: "Lapicera", 
+			price: 150.5, 
+			thumbnail: `http://www.google.com` 
+		},
+		{ 
+			title: "Lapiz", 
+			price: 50.65, 
+			thumbnail: `http://www.bing.com` 
+		},
+		{
+			title: "Goma de borrar",
+			price: 20.75,
+			thumbnail: `http://www.yahoo.com`,
+		},
+	]);
+
+	console.log(await knex.from("productos").select("*"));
+	await knex.destroy();
+};
+
+init();
